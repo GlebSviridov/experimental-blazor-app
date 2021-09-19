4f24a835353dc2e1c7cd4af81c10bb2d9e558841
Sviridov Gleb <gsviridov@ozon.ru>
2021-09-19 16:56:11 +0300

    first implementation of life game

--- Program.cs
+++ Program.cs
@@ -13,8 +13,8 @@ namespace blazor_app
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services
+                .AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             await builder.Build().RunAsync();
         }
     }
